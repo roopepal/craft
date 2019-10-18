9b3658011d2b62f733f902d99c11eaf072c6f2f8
Roope Palomäki <roope.palomaki@aalto.fi>
2019-10-18 19:23:38 +0300

    Add simple fog

--- Craft/fragment.glsl
+++ Craft/fragment.glsl
@@ -2,10 +2,15 @@
 
 in vec4 texcoord;
 uniform sampler2D u_texture;
-out vec4 out_Color;
+out vec4 out_color;
+
+const vec4 fog_color = vec4(0.6, 0.8, 1.0, 1.0);
+const float fog_density = 0.00005;
 
 void main()
 {
+	vec4 color;
+
 	float w = 6.0; // columns in atlas
 	float h = 3.0; // rows in atlas
 	
@@ -14,12 +19,17 @@ void main()
 		float type = abs(texcoord.w);
 		float x = clamp(fract(texcoord.x), 0.01, 0.99) / w + mod((type - 1.0), w) / w;
 		float y = clamp(fract(texcoord.z), 0.01, 0.99) / h + int((type - 1.0 - 0.001) / w) / h;
-		out_Color = texture2D(u_texture, vec2(x, y));
+		color = texture2D(u_texture, vec2(x, y));
 	}
 	else
 	{
 		float x = clamp(fract(texcoord.x + texcoord.z), 0.01, 0.99) / w + mod((texcoord.w - 1.0), w) / w;
 		float y = clamp(fract(texcoord.y), 0.01, 0.99) / h + int((texcoord.w - 1.0 - 0.001) / w) / h;
-		out_Color = texture2D(u_texture, vec2(x, y));
+		color = texture2D(u_texture, vec2(x, y)) * 0.8;
 	}
+
+	float z = gl_FragCoord.z / gl_FragCoord.w; // distance to camera
+	float fog_strength = clamp(exp(-fog_density * z * z), 0.2, 1);
+
+	out_color = mix(fog_color, color, fog_strength);
 }
