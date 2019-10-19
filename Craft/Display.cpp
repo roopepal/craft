c313e36d54f0da6a9a4b54dcfc33bdf541f0ded7
Roope Palomäki <roope.palomaki@me.com>
2019-10-19 17:36:09 +0300

    Update texture interpolation to GL_NEAREST

--- Craft/Display.cpp
+++ Craft/Display.cpp
@@ -218,7 +218,8 @@ void Display::load_texture_from_file(const char* path)
 
 	glGenTextures(1, &texture);
 	glBindTexture(GL_TEXTURE_2D, texture);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texture_w, texture_h, 0, GL_RGBA, GL_UNSIGNED_BYTE, &texture_image[0]);
 }
 
