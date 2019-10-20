600dcf2457e3b66f89d9efa8858b6d7401e17e48
Roope Palomäki <roope.palomaki@me.com>
2019-10-20 18:56:28 +0300

    Restructure rendering to fix chunk crossings

--- Craft/Chunk.cpp
+++ Craft/Chunk.cpp
@@ -13,183 +13,42 @@ typedef glm::tvec4<GLbyte> byte4;
 Chunk::Chunk()
 {
 	memset(blocks, 0, sizeof(blocks));
-	memset(vertices_opaque, 0, sizeof(vertices_opaque));
-	memset(vertices_transparent, 0, sizeof(vertices_transparent));
-	n_opaque = 0;
-	n_transparent = 0;
-	changed = true;
-	glGenBuffers(1, &vbo_opaque);
-	glGenBuffers(1, &vbo_transparent);
-}
-
-Chunk::~Chunk()
-{
-	glDeleteBuffers(1, &vbo_opaque);
-	glDeleteBuffers(1, &vbo_transparent);
 }
 
 int Chunk::get(int x, int y, int z)
 {
-	return blocks[x][y][y];
+	return blocks[x][y][z];
 }
 
 void Chunk::set(int x, int y, int z, int block_type)
 {
 	blocks[x][y][z] = block_type;
-	changed = true;
-}
-
-void Chunk::update()
-{
-	changed = false;
-
-	n_opaque = 0;
-	n_transparent = 0;
-
-	for (int x = 0; x < BLOCKS_X; ++x)
-	{
-		for (int y = 0; y < BLOCKS_Y; ++y)
-		{
-			for (int z = 0; z < BLOCKS_Z; ++z)
-			{
-				int block_type = blocks[x][y][z];
-				int block_type_top = -block_type;
-				int block_type_bottom = -block_type;
-				
-				// grass top and bottom
-				if (block_type == 1)
-				{
-					block_type_top = -15;
-					block_type_bottom = -2;
-				}
-
-				if (!block_type)
-				{
-					continue;
-				}
-				
-				bool self_tp = block_type == 9 || block_type == 4;
-				
-				// has neighbor
-				int x_next = x < BLOCKS_X - 1 ? blocks[x + 1][y][z] : 0;
-				int y_next = y < BLOCKS_Y - 1 ? blocks[x][y + 1][z] : 0;
-				int z_next = z < BLOCKS_Z - 1 ? blocks[x][y][z + 1] : 0;
-				int x_prev = x > 0 ? blocks[x - 1][y][z] : 0;
-				int y_prev = y > 0 ? blocks[x][y - 1][z] : 0;
-				int z_prev = z > 0 ? blocks[x][y][z - 1] : 0;
-
-				// has transparent neighbor
-				int x_next_tp = x_next == 9 || x_next == 4;
-				int y_next_tp = y_next == 9 || y_next == 4;
-				int z_next_tp = z_next == 9 || z_next == 4;
-				int x_prev_tp = x_prev == 9 || x_prev == 4;
-				int y_prev_tp = y_prev == 9 || y_prev == 4;
-				int z_prev_tp = z_prev == 9 || z_prev == 4;
-
-				byte4* vertices = self_tp ? vertices_transparent : vertices_opaque;
-				int &n_vertices = self_tp ? n_transparent : n_opaque;
-
-				// from -x
-				if ((x == 0 || !x_prev || x_prev_tp) && !(self_tp && x_prev))
-				{
-					vertices[n_vertices++] = byte4(x, y, z, block_type);
-					vertices[n_vertices++] = byte4(x, y, z + 1, block_type);
-					vertices[n_vertices++] = byte4(x, y + 1, z, block_type);
-					vertices[n_vertices++] = byte4(x, y + 1, z, block_type);
-					vertices[n_vertices++] = byte4(x, y, z + 1, block_type);
-					vertices[n_vertices++] = byte4(x, y + 1, z + 1, block_type);
-				}
-
-				// from +x
-				if ((x == BLOCKS_X - 1 || !x_next || x_next_tp) && !(self_tp && x_next))
-				{
-					vertices[n_vertices++] = byte4(x + 1, y, z, block_type);
-					vertices[n_vertices++] = byte4(x + 1, y + 1, z, block_type);
-					vertices[n_vertices++] = byte4(x + 1, y, z + 1, block_type);
-					vertices[n_vertices++] = byte4(x + 1, y, z + 1, block_type);
-					vertices[n_vertices++] = byte4(x + 1, y + 1, z, block_type);
-					vertices[n_vertices++] = byte4(x + 1, y + 1, z + 1, block_type);
-				}
-				
-				// from -y
-				if ((y == 0 || !y_prev || y_prev_tp) && !(self_tp && y_prev))
-				{
-					vertices[n_vertices++] = byte4(x, y, z, block_type_bottom);
-					vertices[n_vertices++] = byte4(x + 1, y, z, block_type_bottom);
-					vertices[n_vertices++] = byte4(x + 1, y, z + 1, block_type_bottom);
-					vertices[n_vertices++] = byte4(x + 1, y, z + 1, block_type_bottom);
-					vertices[n_vertices++] = byte4(x, y, z + 1, block_type_bottom);
-					vertices[n_vertices++] = byte4(x, y, z, block_type_bottom);
-				}
-				
-
-				// from +y
-				if ((y == BLOCKS_Y - 1 || !y_next || y_next_tp) && !(self_tp && y_next))
-				{
-					vertices[n_vertices++] = byte4(x, y + 1, z, block_type_top);
-					vertices[n_vertices++] = byte4(x + 1, y + 1, z + 1, block_type_top);
-					vertices[n_vertices++] = byte4(x + 1, y + 1, z, block_type_top);
-					vertices[n_vertices++] = byte4(x, y + 1, z, block_type_top);
-					vertices[n_vertices++] = byte4(x, y + 1, z + 1, block_type_top);
-					vertices[n_vertices++] = byte4(x + 1, y + 1, z + 1, block_type_top);
-				}
-
-				// from -z
-				if ((z == 0 || !z_prev || z_prev_tp) && !(self_tp && z_prev))
-				{
-					vertices[n_vertices++] = byte4(x, y, z, block_type);
-					vertices[n_vertices++] = byte4(x, y + 1, z, block_type);
-					vertices[n_vertices++] = byte4(x + 1, y + 1, z, block_type);
-					vertices[n_vertices++] = byte4(x + 1, y + 1, z, block_type);
-					vertices[n_vertices++] = byte4(x + 1, y, z, block_type);
-					vertices[n_vertices++] = byte4(x, y, z, block_type);
-				}
-				
-
-				// from +z
-				if ((z == BLOCKS_Z - 1 || !z_next || z_next_tp) && !(self_tp && z_next))
-				{
-					vertices[n_vertices++] = byte4(x, y, z + 1, block_type);
-					vertices[n_vertices++] = byte4(x + 1, y, z + 1, block_type);
-					vertices[n_vertices++] = byte4(x + 1, y + 1, z + 1, block_type);
-					vertices[n_vertices++] = byte4(x + 1, y + 1, z + 1, block_type);
-					vertices[n_vertices++] = byte4(x, y + 1, z + 1, block_type);
-					vertices[n_vertices++] = byte4(x, y, z + 1, block_type);
-				}
-			}
-		}
-	}
-
-	glBindBuffer(GL_ARRAY_BUFFER, vbo_opaque);
-	glBufferData(GL_ARRAY_BUFFER, n_opaque * sizeof(byte4), vertices_opaque, GL_STATIC_DRAW);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo_transparent);
-	glBufferData(GL_ARRAY_BUFFER, n_transparent * sizeof(byte4), vertices_transparent, GL_STATIC_DRAW);
-}
-
-void Chunk::render(Display* display)
-{
-	if (changed)
-	{
-		update();
-	}
-
-	if (n_opaque > 0)
-	{
-		glBindBuffer(GL_ARRAY_BUFFER, vbo_opaque);
-		glVertexAttribPointer(display->a_coord, 4, GL_BYTE, GL_FALSE, 0, 0);
-		glDrawArrays(GL_TRIANGLES, 0, n_opaque);
-	}
-	if (n_transparent > 0)
-	{
-		glBindBuffer(GL_ARRAY_BUFFER, vbo_transparent);
-		glVertexAttribPointer(display->a_coord, 4, GL_BYTE, GL_FALSE, 0, 0);
-		glDrawArrays(GL_TRIANGLES, 0, n_transparent);
-	}
 }
 
 SuperChunk::SuperChunk()
 {
-	memset(c, 0, sizeof c);
+	memset(vertices_opaque, 0, sizeof(vertices_opaque));
+	memset(vertices_transparent, 0, sizeof(vertices_transparent));
+	n_opaque = 0;
+	n_transparent = 0;
+
+	glGenBuffers(1, &vbo_opaque);
+	glGenBuffers(1, &vbo_transparent);
+
+	max_x = CHUNKS_X * BLOCKS_X - 1;
+	max_y = CHUNKS_Y * BLOCKS_Y - 1;
+	max_z = CHUNKS_Z * BLOCKS_Z - 1;
+
+	for (int x = 0; x < CHUNKS_X; ++x)
+	{
+		for (int y = 0; y < CHUNKS_Y; ++y)
+		{
+			for (int z = 0; z < CHUNKS_Z; ++z)
+			{
+				c[x][y][z] = new Chunk();
+			}
+		}
+	}
 }
 
 SuperChunk::~SuperChunk()
@@ -204,6 +63,9 @@ SuperChunk::~SuperChunk()
 			}
 		}
 	}
+
+	glDeleteBuffers(1, &vbo_opaque);
+	glDeleteBuffers(1, &vbo_transparent);
 }
 
 int SuperChunk::get(int x, int y, int z)
@@ -236,28 +98,157 @@ void SuperChunk::set(int x, int y, int z, int block_type)
 	y %= BLOCKS_Y;
 	z %= BLOCKS_Z;
 
-	if (!c[chunk_x][chunk_y][chunk_z])
+	c[chunk_x][chunk_y][chunk_z]->set(x, y, z, block_type);
+
+	changed = true;
+}
+
+void SuperChunk::update()
+{
+	changed = false;
+
+	n_opaque = 0;
+	n_transparent = 0;
+
+	for (int x = 0; x < max_x; ++x)
 	{
-		c[chunk_x][chunk_y][chunk_z] = new Chunk();
+		for (int y = 0; y < max_y; ++y)
+		{
+			for (int z = 0; z < max_z; ++z)
+			{
+				int block_type = get(x, y, z);
+				int block_type_top = -block_type;
+				int block_type_bottom = -block_type;
+
+				// grass top and bottom
+				if (block_type == 1)
+				{
+					block_type_top = -15;
+					block_type_bottom = -2;
+				}
+
+				if (!block_type)
+				{
+					continue;
+				}
+
+				bool self_tp = block_type == 9 || block_type == 4;
+
+				// has neighbor
+				int x_next, y_next, z_next, x_prev, y_prev, z_prev;
+
+				x_next = x == max_x - 1 ? 0 : get(x + 1, y, z);
+				y_next = y == max_y - 1 ? 0 : get(x, y + 1, z);
+				z_next = z == max_z - 1 ? 0 : get(x, y, z + 1);
+				x_prev = x == 0 ? 0 : get(x - 1, y, z);
+				y_prev = y == 0 ? 0 : get(x, y - 1, z);
+				z_prev = z == 0 ? 0 : get(x, y, z - 1);
+
+				// has transparent neighbor
+				int x_next_tp = x_next == 9 || x_next == 4;
+				int y_next_tp = y_next == 9 || y_next == 4;
+				int z_next_tp = z_next == 9 || z_next == 4;
+				int x_prev_tp = x_prev == 9 || x_prev == 4;
+				int y_prev_tp = y_prev == 9 || y_prev == 4;
+				int z_prev_tp = z_prev == 9 || z_prev == 4;
+
+				byte4* vertices = self_tp ? vertices_transparent : vertices_opaque;
+				long int &n_vertices = self_tp ? n_transparent : n_opaque;
+
+				// from -x
+				if ((x == 0 || !x_prev || x_prev_tp) && !(self_tp && x_prev))
+				{
+					vertices[n_vertices++] = byte4(x, y, z, block_type);
+					vertices[n_vertices++] = byte4(x, y, z + 1, block_type);
+					vertices[n_vertices++] = byte4(x, y + 1, z, block_type);
+					vertices[n_vertices++] = byte4(x, y + 1, z, block_type);
+					vertices[n_vertices++] = byte4(x, y, z + 1, block_type);
+					vertices[n_vertices++] = byte4(x, y + 1, z + 1, block_type);
+				}
+
+				// from +x
+				if ((!x_next || x_next_tp) && !(self_tp && x_next))
+				{
+					vertices[n_vertices++] = byte4(x + 1, y, z, block_type);
+					vertices[n_vertices++] = byte4(x + 1, y + 1, z, block_type);
+					vertices[n_vertices++] = byte4(x + 1, y, z + 1, block_type);
+					vertices[n_vertices++] = byte4(x + 1, y, z + 1, block_type);
+					vertices[n_vertices++] = byte4(x + 1, y + 1, z, block_type);
+					vertices[n_vertices++] = byte4(x + 1, y + 1, z + 1, block_type);
+				}
+
+				// from -y
+				if ((!y_prev || y_prev_tp) && !(self_tp && y_prev))
+				{
+					vertices[n_vertices++] = byte4(x, y, z, block_type_bottom);
+					vertices[n_vertices++] = byte4(x + 1, y, z, block_type_bottom);
+					vertices[n_vertices++] = byte4(x + 1, y, z + 1, block_type_bottom);
+					vertices[n_vertices++] = byte4(x + 1, y, z + 1, block_type_bottom);
+					vertices[n_vertices++] = byte4(x, y, z + 1, block_type_bottom);
+					vertices[n_vertices++] = byte4(x, y, z, block_type_bottom);
+				}
+
+				// from +y
+				if ((!y_next || y_next_tp) && !(self_tp && y_next))
+				{
+					vertices[n_vertices++] = byte4(x, y + 1, z, block_type_top);
+					vertices[n_vertices++] = byte4(x + 1, y + 1, z + 1, block_type_top);
+					vertices[n_vertices++] = byte4(x + 1, y + 1, z, block_type_top);
+					vertices[n_vertices++] = byte4(x, y + 1, z, block_type_top);
+					vertices[n_vertices++] = byte4(x, y + 1, z + 1, block_type_top);
+					vertices[n_vertices++] = byte4(x + 1, y + 1, z + 1, block_type_top);
+				}
+
+				// from -z
+				if ((!z_prev || z_prev_tp) && !(self_tp && z_prev))
+				{
+					vertices[n_vertices++] = byte4(x, y, z, block_type);
+					vertices[n_vertices++] = byte4(x, y + 1, z, block_type);
+					vertices[n_vertices++] = byte4(x + 1, y + 1, z, block_type);
+					vertices[n_vertices++] = byte4(x + 1, y + 1, z, block_type);
+					vertices[n_vertices++] = byte4(x + 1, y, z, block_type);
+					vertices[n_vertices++] = byte4(x, y, z, block_type);
+				}
+
+				// from +z
+				if ((!z_next || z_next_tp) && !(self_tp && z_next))
+				{
+					vertices[n_vertices++] = byte4(x, y, z + 1, block_type);
+					vertices[n_vertices++] = byte4(x + 1, y, z + 1, block_type);
+					vertices[n_vertices++] = byte4(x + 1, y + 1, z + 1, block_type);
+					vertices[n_vertices++] = byte4(x + 1, y + 1, z + 1, block_type);
+					vertices[n_vertices++] = byte4(x, y + 1, z + 1, block_type);
+					vertices[n_vertices++] = byte4(x, y, z + 1, block_type);
+				}
+			}
+		}
 	}
 
-	c[chunk_x][chunk_y][chunk_z]->set(x, y, z, block_type);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo_opaque);
+	glBufferData(GL_ARRAY_BUFFER, n_opaque * sizeof(byte4), vertices_opaque, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo_transparent);
+	glBufferData(GL_ARRAY_BUFFER, n_transparent * sizeof(byte4), vertices_transparent, GL_STATIC_DRAW);
 }
 
 void SuperChunk::render(Display* display)
 {
-	for (int x = 0; x < CHUNKS_X; ++x)
+	if (changed)
 	{
-		for (int y = 0; y < CHUNKS_Y; ++y)
-		{
-			for (int z = 0; z < CHUNKS_Z; ++z)
-			{
-				if (c[x][y][z])
-				{
-					display->set_model_translation(x*BLOCKS_X, y*BLOCKS_Y, z*BLOCKS_Z);
-					c[x][y][z]->render(display);
-				}
-			}
-		}
+		update();
+	}
+
+	display->set_model_translation(0, 0, 0);
+
+	if (n_opaque > 0)
+	{
+		glBindBuffer(GL_ARRAY_BUFFER, vbo_opaque);
+		glVertexAttribPointer(display->a_coord, 4, GL_BYTE, GL_FALSE, 0, 0);
+		glDrawArrays(GL_TRIANGLES, 0, n_opaque);
+	}
+	if (n_transparent > 0)
+	{
+		glBindBuffer(GL_ARRAY_BUFFER, vbo_transparent);
+		glVertexAttribPointer(display->a_coord, 4, GL_BYTE, GL_FALSE, 0, 0);
+		glDrawArrays(GL_TRIANGLES, 0, n_transparent);
 	}
 }
--- Craft/Chunk.h
+++ Craft/Chunk.h
@@ -9,40 +9,45 @@
 constexpr int BLOCKS_X = 16;
 constexpr int BLOCKS_Y = 16;
 constexpr int BLOCKS_Z = 16;
-constexpr int CHUNKS_X = 3;
-constexpr int CHUNKS_Y = 1;
-constexpr int CHUNKS_Z = 3;
+constexpr int CHUNKS_X = 8;
+constexpr int CHUNKS_Y = 3;
+constexpr int CHUNKS_Z = 8;
 
 typedef glm::tvec4<GLbyte> byte4;
+
+struct SuperChunk;
 
 struct Chunk
 {
 	int blocks[BLOCKS_X][BLOCKS_Y][BLOCKS_Z];
-	byte4 vertices_opaque[BLOCKS_X * BLOCKS_Y * BLOCKS_Z * 6 * 6];
-	byte4 vertices_transparent[BLOCKS_X * BLOCKS_Y * BLOCKS_Z * 6 * 6];
-	int n_opaque;
-	int n_transparent;
-	GLuint vbo_opaque;
-	GLuint vbo_transparent;
-	bool changed;
 
 	int get(int x, int y, int z);
 	void set(int x, int y, int z, int block_type);
 
-	void update();
-	void render(Display* display);
-
 	Chunk();
-	~Chunk();
 };
 
 struct SuperChunk
 {
 	Chunk* c[CHUNKS_X][CHUNKS_Y][CHUNKS_Z];
 
+	int max_x;
+	int max_y;
+	int max_z;
+
+	byte4 vertices_opaque[BLOCKS_X * CHUNKS_X * BLOCKS_Y * CHUNKS_Y * BLOCKS_Z * CHUNKS_Z * 6 * 6];
+	byte4 vertices_transparent[BLOCKS_X * BLOCKS_Y * BLOCKS_Z * 6 * 6];
+	long int n_opaque;
+	long int n_transparent;
+	GLuint vbo_opaque;
+	GLuint vbo_transparent;
+
+	bool changed;
+
 	int get(int x, int y, int z);
 	void set(int x, int y, int z, int block_type);
 
+	void update();
 	void render(Display* display);
 
 	SuperChunk();
